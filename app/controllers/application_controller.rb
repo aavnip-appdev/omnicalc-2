a55ed68a93a2a288130211ac699e757d02889898
aavnip <aavnip@chicagobooth.edu>
2022-02-01 16:38:06 +0000

    Omnicalc WIP 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
 
+def add_form
+  render({:template => "math/add_form.html.erb"})
+end
+
+def add_results
+  @first = params.fetch("first_num").to_f
+  @second = params.fetch("second_num").to_f
+  @result = @first + @second
+
+  render({:template => "math/add_results.html.erb"})
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/add",{:controller => "application", :action => "add_form"})
+  get("/wizard_add",{:controller => "application", :action => "add_results"})
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
